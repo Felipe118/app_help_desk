406b6ca024f6f4f32d82c35d7bb09e0bfd9bb511
ServidorMix@servidormix <lipebezerraa@gmail.com>
2021-02-24 12:56:50 -0300

    Criando o registra_chamado.php para salvar informações sobre o chamado do problema em um arquivi.txt criado pelo php

---
+++ registra_chamado.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+$arquivo = fopen('arquivo.hd','a');
+$titulo = str_replace('#','-', $_POST['titulo'] );
+$categoria = str_replace('#','-', $_POST['categoria'] );
+$descricao  = str_replace('#','-', $_POST['descricao'] );
+
+$texto = $titulo. " # " . $categoria  .  " # " . $descricao. PHP_EOL; 
+
+fwrite($arquivo, $texto);
+
+fclose($arquivo);
+
+header('Location: abrir_chamado.php')
+
+?>
