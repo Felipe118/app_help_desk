53974805c1fa9901ff49dc35a7ecec6ca937d04d
Luis Felipe <lipebezerraa@gmail.com>
2021-02-25 21:37:52 -0300

    Vinculando perfis ao seus determinados chamados adm ver todos pessoas só ve o seu chamado

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -2,14 +2,29 @@
   require_once "validador_acesso.php";
 ?>
 <?php
+
+
   //chamados
   $chamados = array();
   $arquivo = fopen('arquivo.hd', 'r');
 
   while(!feof($arquivo)){
 
-    $registros = fgets($arquivo);
-    $chamados[] = $registros;
+    $registro = fgets($arquivo);
+    $registro_detalhes = explode('#', $registro);
+    if($_SESSION['perfil_id'] == 2) {
+
+      //se usuário autenticado não for o usuário de abertura do chamado então não faz nada
+      if($_SESSION['id'] != $registro_detalhes[0]) {
+        continue; //não faz nada
+
+      } else {
+        $chamados[] = $registro; //adiciona o registro do arquivo ao array $chamados
+      }
+
+    } else {
+      $chamados[] = $registro; //adiciona o registro do arquivo ao array $chamados
+    }
 
   }
 
@@ -60,11 +75,14 @@
               
               <?php foreach($chamados as $chamado ) {?>
               <?php 
-                $chamado_dados = explode('#', $chamado);
-                  
-                if(count($chamado_dados) < 3){
-                  continue;
-                }
+
+
+              $chamado_dados = explode('#', $chamado);
+
+              //não existe detalhes do chamado se ele não estiver completo
+                    if(count($chamado_dados) < 3) {
+                         continue;
+                        }
 
                 
                 
@@ -72,9 +90,9 @@
 
               <div class="card mb-3 bg-light">
                 <div class="card-body">
-                  <h5 class="card-title"><?= $chamado_dados[0] ?></h5>
-                  <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[1]?></h6>
-                  <p class="card-text"><?=  $chamado_dados[2]?></p>
+                  <h5 class="card-title"><?= $chamado_dados[1] ?></h5>
+                  <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[2]?></h6>
+                  <p class="card-text"><?=  $chamado_dados[3]?></p>
 
                 </div>
               </div>
--- valida_login.php
+++ valida_login.php
@@ -3,13 +3,17 @@
     print_r($_SESSION);
 //VARIAVEL QUE VERIFICA SE A AUTENTICAÇÃO FOI REALIZADA
 $usuario_autenticado = false;
+$usuario_id = null;
+$usuario_perfil_id = null;
+
+$perfis = array(1 => 'Administrativo', 2 => 'Usuário');
 
 //USUARIOS DO SISTEMA
 $usuarios_app = array(
-    array('id' => 'email' => 'adm@teste.com.br', 'senha' => '123456'),
-    array('id' =>  'email' => 'user@teste.com.br', 'senha' => 'abcd')
-    array('id' =>  'email' => 'luis@teste.com.br', 'senha' => '123')
-    array('id' =>  'email' => 'ze@teste.com.br', 'senha' => 'ze')
+    array('id' => 1, 'email' => 'adm@teste.com.br', 'senha' => '123456', 'perfil_id' => 1),
+    array('id' => 2, 'email' => 'user@teste.com.br', 'senha' => 'abcd','perfil_id' => 1 ),
+    array('id' => 3, 'email' => 'luis@teste.com.br', 'senha' => '123' ,'perfil_id' => 2),
+    array('id' => 4, 'email' => 'ze@teste.com.br', 'senha' => 'ze' , 'perfil_id' => 2)
 );
 
 
@@ -17,6 +21,8 @@ foreach($usuarios_app as $user){
    
     if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
         $usuario_autenticado = true;
+        $usuario_id = $user['id'];
+        $usuario_perfil_id  = $user['perfil_id'];
     }
 }
     
@@ -24,6 +30,8 @@ if($usuario_autenticado){
     echo 'Usuário autenticado.';
 
     $_SESSION['autenticado'] = 'SIM';
+    $_SESSION['id'] = $usuario_id;
+    $_SESSION['perfil_id'] = $usuario_perfil_id ;
     header('Location: home.php');
   }else{  
     $_SESSION['autenticado'] = 'NÃO';
