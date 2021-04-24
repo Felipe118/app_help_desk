f04a8704377bab57871864fa3876584245d376f2
ServidorMix@servidormix <lipebezerraa@gmail.com>
2021-04-24 10:09:38 -0300

    Adicionando o conexao

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+
+class Conexao{
+    private $host = 'localhost';
+    private $dbname = 'app_help_desk';
+    private $user = 'root';
+    private $pass = '';
+
+    public function connect(){
+        
+    }
+}
+
+?>
